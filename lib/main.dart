b9d37fb977828ec1c60e683d24d5417991285f06
Kamil Micota <kamilmicota42@gmail.com>
2023-03-06 09:18:29 +0100

    botCurvNavBarWLabels problem with selected item

--- lib/main.dart
+++ lib/main.dart
@@ -1,53 +1,85 @@
-import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 import 'package:flutter/material.dart';
+import 'package:curved_labeled_navigation_bar/curved_navigation_bar.dart';
+import 'package:curved_labeled_navigation_bar/curved_navigation_bar_item.dart';
 
 void main() {
   runApp(const MainApp());
 }
 
-class MainApp extends StatefulWidget {
+class MainApp extends StatelessWidget {
   const MainApp({super.key});
 
   @override
-  State<MainApp> createState() => _MainAppState();
-}
-
-class _MainAppState extends State<MainApp> {
-  int navBarIndex = 0;
-
-  @override
   Widget build(BuildContext context) {
-    final navBarItems = <Widget>[
-      Icon((navBarIndex == 0) ? Icons.home_outlined : Icons.home),
-      Icon((navBarIndex == 1) ? Icons.map_outlined : Icons.map),
-      Icon((navBarIndex == 2) ? Icons.notifications_outlined : Icons.notifications),
-    ];
-
-    return  MaterialApp(
-      debugShowCheckedModeBanner: false,
+    return const MaterialApp(
       home: Scaffold(
-        body: const Center(
+        body: Center(
           child: Text('Hello World!'),
         ),
-        extendBody: true, // if the background of navbar isnt transparent, happens when theres picture in the background
-        bottomNavigationBar: Theme(
-          data: Theme.of(context).copyWith(
-            iconTheme: const IconThemeData(color: Colors.white),
-            
+        bottomNavigationBar: BotNavBar(),
+      ),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class BotNavBar extends StatefulWidget {
+  const BotNavBar({super.key});
+
+  @override
+  State<BotNavBar> createState() => _BotNavBarState();
+}
+
+class _BotNavBarState extends State<BotNavBar> {
+  @override
+  Widget build(BuildContext context) {
+    int navBarIndex = 0;
+
+    const navBarColor = Color.fromRGBO(17, 44, 72, 1);
+    const buttonBackgroundColor = Color.fromRGBO(251, 123, 52, 1);
+    const navBarLabelStyle =
+        TextStyle(fontWeight: FontWeight.bold, color: Colors.white);
+
+    return Scaffold(
+      bottomNavigationBar: CurvedNavigationBar(
+        backgroundColor: Colors.transparent,
+        items: [
+          CurvedNavigationBarItem(
+            child: Icon(
+              Icons.home,
+              color: navBarIndex == 0 ? Colors.white : Colors.grey,
+            ),
+            label: 'Home',
+            labelStyle: navBarLabelStyle,
           ),
-          child: CurvedNavigationBar(
-            height: 60,
-            animationCurve: Curves.easeInOut,
-            animationDuration: const Duration(milliseconds: 200),
-            index: navBarIndex,
-            items: navBarItems,
-            color: const Color.fromRGBO(17, 44, 72, 1),
-            buttonBackgroundColor: const Color.fromRGBO(17, 44, 72, 1),
-            
-            backgroundColor: Colors.transparent,
-            onTap: (navBarIndex) => setState(() => this.navBarIndex = navBarIndex),
+          CurvedNavigationBarItem(
+            child: Icon(
+              Icons.map,
+              color: navBarIndex == 1 ? Colors.white : Colors.grey,
+            ),
+            label: 'Navigation',
+            labelStyle: navBarLabelStyle,
           ),
-        ),
+          CurvedNavigationBarItem(
+            child: Icon(
+              Icons.notifications,
+              color: navBarIndex == 2 ? Colors.white : Colors.grey,
+            ),
+            label: 'Notifications',
+            labelStyle: navBarLabelStyle,
+          ),
+        ],
+        onTap: (index) {
+          setState(() {
+            navBarIndex = index;
+            print(navBarIndex);
+          });
+        },
+        color: navBarColor,
+        buttonBackgroundColor: buttonBackgroundColor,
+        animationCurve: Curves.easeInOutQuint,
+        animationDuration: const Duration(milliseconds: 300),
+        height: 75,
       ),
     );
   }
